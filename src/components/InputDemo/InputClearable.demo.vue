ecda7554a5bbc4171f26f16d74ee2dade0a912b9
denghua <denghua.fm@foxmail.com>
2022-12-16 20:10:33 +0800

    feat: supplement input component 'clear' event and event handle

--- src/components/InputDemo/InputClearable.demo.vue
+++ src/components/InputDemo/InputClearable.demo.vue
@@ -6,6 +6,7 @@
   <Input
     v-model:value="value"
     placeholder="clearable"
+    @clear="clearHandle"
     clearable
   />
 </template>
@@ -15,4 +16,9 @@ import { ref } from "vue";
 import { Input } from "poor-ui";
 
 const value = ref('');
+
+const clearHandle = () => {
+  console.log('input value is clean');
+}
+
 </script>
